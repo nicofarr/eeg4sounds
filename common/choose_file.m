8c7afff3c2e48fcd78032e7f2fb05866e285882e
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-13 13:40:13 +0200

    new file to choose file

---
+++ common/choose_file.m
@@ -0,0 +1,5 @@
+function [currentfile] = choose_file(datadir)
+[curfile,path2file] = uigetfile({[datadir '/*.mff']},'Choisissez le fichier à analyser');
+
+currentfile = [path2file,curfile];
+disp(['File : ' currentfile])
