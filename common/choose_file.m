5121cca82b859a2d02f7825501c952677d0cb2ee
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-13 15:36:30 +0200

    added subject ID extraction

--- common/choose_file.m
+++ common/choose_file.m
@@ -1,5 +1,6 @@
-function [currentfile] = choose_file(datadir)
+function [currentfile,subjid] = choose_file(datadir)
 [curfile,path2file] = uigetfile({[datadir '/*.mff']},'Choisissez le fichier à analyser');
 
 currentfile = [path2file,curfile];
+subjid = curfile(1:4);
 disp(['File : ' currentfile])
