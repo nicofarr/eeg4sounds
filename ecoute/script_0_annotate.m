4b44bd6b5f915a6244f03829de3f2d28666650c8
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-13 15:50:58 +0200

    finalizing the saving of files

--- ecoute/script_0_annotate.m
+++ ecoute/script_0_annotate.m
@@ -2,11 +2,25 @@ addpath ../common/
 
 check_set_datadir;
 
+check_set_resultdir;
+
 [file_ecoute,subjid] = choose_file(datadir);
 
+resultfile = [resultdir '/ecoute/annot/' subjid '.mat'];
+
+if exist(resultfile,'file')
+    disp(['WARNING : Subject ' subjid ' has already been processed.']) 
+    
+    decision = input('Process anyway? ( n to abort) ','s');
+    if decision == 'n'
+        error('ABORTED')
+    end
+end
+        
+    
 close all
 
-%%% Reading the header to fetch the total number of samples 
+%%% Reading the header to fetch the total number of samples
 header_struct = ft_read_header(file_ecoute);
 nsamples = header_struct.nSamples;
 
@@ -14,9 +28,9 @@ nsamples = header_struct.nSamples;
 cfg=[];
 cfg.dataset=file_ecoute;
 cfg.dataformat = 'egi_mff_v2';
-cfg.trialdef.eventtype = 'comm'; 
+cfg.trialdef.eventtype = 'comm';
 %%% the "comm" trials seem to correspo to the ones that were added manually
-%%% during the experiment 
+%%% during the experiment
 
 %%% Here we always extract 2 minutes minus 5 seconds, and we will redefine
 %%% trials afterwards by (1) adding an offset of 5 seconds and (2)
@@ -31,9 +45,9 @@ cfg.eventformat = 'egi_mff_v2';
 
 cfg = ft_definetrial(cfg);
 
-%%%% Noter ici le nombre detrials 
+%%%% Noter ici le nombre detrials
 clear trials
-trials = cfg.trl; 
+trials = cfg.trl;
 
 trials(:,1)=trials(:,1) + 5000;
 
@@ -44,13 +58,13 @@ nbtrials = plot_ann_trials(trials,nsamples,fig);
 
 
 
-%%%% Section to ADD new trials 
+%%%% Section to ADD new trials
 addtrial = input('Do you want to add a trial?','s');
 while addtrial == 'y'
-    disp('Current trial onsets (in samples) are : ') 
+    disp('Current trial onsets (in samples) are : ')
     disp(trials(:,1))
     
-    disp('Current trial offsets (in samples) are : ') 
+    disp('Current trial offsets (in samples) are : ')
     disp(trials(:,2))
     
     
@@ -66,17 +80,17 @@ while addtrial == 'y'
     end
     
     
-    disp('Updated trial onsets (in samples) are : ') 
+    disp('Updated trial onsets (in samples) are : ')
     disp(trials(:,1))
     
-    disp('Updated trial offsets (in samples) are : ') 
+    disp('Updated trial offsets (in samples) are : ')
     disp(trials(:,2))
     
     
     nbtrials = plot_ann_trials(trials,nsamples,fig);
-
+    
     addtrial = input('Do you want to add another trial?','s');
-
+    
 end
 
 
@@ -86,7 +100,7 @@ end
 movetrial = input('Do you want to delay a trial?','s');
 
 while movetrial == 'y'
-    disp('Current trial onsets (in samples) are : ') 
+    disp('Current trial onsets (in samples) are : ')
     disp(trials(:,1))
     
     onset2move = input(['Which trial do you want to move ? : ', num2str((1:nbtrials))]);
@@ -98,13 +112,13 @@ while movetrial == 'y'
         trials(onset2move,1)+samples2add+newduration, 0];
     
     
-    disp('Updated trial onsets (in samples) are : ') 
+    disp('Updated trial onsets (in samples) are : ')
     disp(trials(:,1))
     
     nbtrials = plot_ann_trials(trials,nsamples,fig);
-
+    
     movetrial = input('Do you want to move another trial?','s');
-
+    
 end
 
 
@@ -122,65 +136,71 @@ load trialnamesref
 trialnameval = 0;
 clear trialnames
 
-while trialnameval == 0 
-
+while trialnameval == 0
+    
     disp([num2str(nbtrials) ' trials for this subject'])
-
+    
     disp('TRIAL NAMING')
-    disp('Possible names are : ') 
+    disp('Possible names are : ')
     disp(trialnamesref)
     for i=1:nbtrials
         strinput=input(['Name Trial ', num2str(i) ':'],'s');
-
+        
         testval = strcmp(trialnamesref,strinput);
         
         while sum(testval) == 0
             disp('Name not allowed')
-            disp('Possible names are : ') 
+            disp('Possible names are : ')
             disp(trialnamesref)
             strinput=input(['Name Trial ', num2str(i) ':'],'s');
             testval = strcmp(trialnamesref,strinput);
         end
         
         trialnames{i} = strinput;
-
+        
     end
-
-    disp('TRIAL NAMES : ' ) 
+    
+    disp('TRIAL NAMES : ' )
     disp(trialnames)
     
     trialnameval = 1;
-% 
-%     for i=1:nbtrials
-% 
-%         test = strcmp(trialnamesref{i},trialnames);
-%         if sum(test) < 1
-%             disp('PROBLEM IN TRIAL NAMES')
-%             disp(['Name ' trialnames{i} ' for trial ' num2str(i) ' is not allowed'])
-%             trialnameval = 0;
-%             disp('NAMING WILL RESTART NOW')
-%             pause(2)
-%             break
-%         elseif sum(test) > 1
-% 
-%             disp('PROBLEM IN TRIAL NAMES')
-%             disp(['Name ' trialnames{i} ' is used more than once'])
-%             disp(trialnames)
-%             trialnameval = 0;
-%             disp('NAMING WILL RESTART NOW')
-%             pause(2)
-%             clc
-%             break
-%         end
-%     end
-
+    %
+    %     for i=1:nbtrials
+    %
+    %         test = strcmp(trialnamesref{i},trialnames);
+    %         if sum(test) < 1
+    %             disp('PROBLEM IN TRIAL NAMES')
+    %             disp(['Name ' trialnames{i} ' for trial ' num2str(i) ' is not allowed'])
+    %             trialnameval = 0;
+    %             disp('NAMING WILL RESTART NOW')
+    %             pause(2)
+    %             break
+    %         elseif sum(test) > 1
+    %
+    %             disp('PROBLEM IN TRIAL NAMES')
+    %             disp(['Name ' trialnames{i} ' is used more than once'])
+    %             disp(trialnames)
+    %             trialnameval = 0;
+    %             disp('NAMING WILL RESTART NOW')
+    %             pause(2)
+    %             clc
+    %             break
+    %         end
+    %     end
+    
 end
-disp('FINISHED TRIAL NAMING') 
+disp('FINISHED TRIAL NAMING')
 
-disp('SAVING...') 
+disp('SAVING...')
+
+if ~exist([resultdir '/ecoute/annot'],'dir')
+    
+    mkdir([resultdir '/ecoute/annot'])
+    
+end
+
+save(resultfile,'cfg','trialnames')
 
 
-
-
-disp('SUCESSFUL ! ') 
+disp('SUCESSFUL ! ')
 
