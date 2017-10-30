c8ddb8d165e24916c03f8fc9f614cff47f120874
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-30 15:35:12 +0100

    dealt with windows / mac compatbility

--- common/choose_file.m
+++ common/choose_file.m
@@ -1,6 +1,18 @@
 function [currentfile,subjid] = choose_file(datadir)
-[curfile,path2file] = uigetfile({[datadir '/*.mff']},'Choisissez le fichier à analyser');
 
-currentfile = [path2file,curfile];
-subjid = curfile(1:4);
-disp(['File : ' currentfile])
+if strcmp(computer,'MACI64')
+    [curfile,path2file] = uigetfile({[datadir '/*.mff']},'Choisissez le fichier à analyser');
+    
+    currentfile = [path2file,curfile];
+    subjid = curfile(1:4);
+    disp(['File : ' currentfile])
+    disp(['Subject ID : ' subjid])
+    
+else
+    
+    [currentfile] = uigetdir(datadir,'Choisissez le fichier à analyser');
+    
+    subjid = currentfile(end-29:end-26);
+    disp(['File : ' currentfile])
+    disp(['Subject ID : ' subjid])
+end
