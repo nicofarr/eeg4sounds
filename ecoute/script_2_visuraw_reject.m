e5393356b1333931a585a89eeb17738cf245b9f1
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-30 16:06:52 +0100

    First version of inspect

---
+++ ecoute/script_2_visuraw_reject.m
@@ -0,0 +1,45 @@
+%% 2 - Rejection d'artefact visuellement identifiables sur des données filtrées 
+%% -- input : données brutes (elec utiles )
+%% -- output : annotations d'artefacts identifiés visuellement
+%% cette étape est semi-automatique
+
+
+[filetoinspect,filepath] = uigetfile([resultdir '/ecoute/import/*.mat']);
+
+%%% Checking whether this file has been already inspected 
+
+
+%%% Create result folder for storing visual artefacts
+
+if ~exist([resultdir '/ecoute/vis_art'],'dir')
+    
+    mkdir([resultdir '/ecoute/vis_art'])
+    
+end
+
+resultfile_visart = [resultdir, '/ecoute/vis_art/', filetoinspect];
+
+if exist(resultfile_visart,'file')
+    disp('File has already been visually inspected : loading results.');
+end
+
+
+
+%%% Defining the cfg for inspection 
+
+cfg = [];
+
+visdata = load([filepath,filetoinspect],'data');
+
+cfg.viewmode = 'vertical'; %%%% remplacer par butterfly pour avoir les electrodes superposées
+%%% Paramètres de préprocessing uniquement pour la visu
+cfg.preproc.bpfilter = 'yes';
+cfg.preproc.bpfreq = [0.3 70];
+
+cfg.blocksize = 10; %%% by blocks of 10 seconds 
+cfg.channel = [1:10];
+
+cfg.preproc.dftfilter = 'yes'; %%% Ceci est le notch
+
+%%% Cette fonction démarre l'outil de visualisation
+cfg_visual_clean = ft_databrowser(cfg,visdata.data);
