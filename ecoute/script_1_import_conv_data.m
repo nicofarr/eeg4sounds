43aac16e6d523176043777edd17e964621698006
Nicolas Farrugia <n.farrugia@gold.ac.uk>
2017-10-30 15:10:35 +0100

    first version of script to import and convert raw data into Fieldtrip

---
+++ ecoute/script_1_import_conv_data.m
@@ -0,0 +1,54 @@
+%% 1 - Garder un set d'electrodes défini à l'avance (défini dans common/eleckeep.txt)
+%% -- input : données brutes
+%% -- output : données brutes moins les électrodes rejetées (format mat)
+
+%%% Ce fichier scan le repertoire entier de fichiers annotés, et lance la
+%%% conversion sur chacun d'entre eux
+
+
+%% Load the list of good electrodes
+load_good_elecs;
+
+%% List all annotated files
+annotedfiles = dir([resultdir '/ecoute/annot']);
+%% Remove '.' and '..'
+annotedfiles(1:2) = [];
+
+
+%%% Create result folder
+
+if ~exist([resultdir '/ecoute/import'],'dir')
+    
+    mkdir([resultdir '/ecoute/import'])
+    
+end
+
+%%% Loop over all annoted files
+
+for ind = 1:size(annotedfiles,1)
+    
+    curfile = annotedfiles(ind).name;
+    
+    filepath = [resultdir,'/ecoute/annot/' annotedfiles(ind).name];
+    
+    cfgloaded = load(filepath,'cfg');
+    
+    cfgpreproc = cfgloaded.cfg;
+   
+    %%% Setting up the resultfile
+    
+    resultfile = [resultdir '/ecoute/import/' curfile];
+    
+    %%% Setting up parameters for "preprocessing"
+    %%% We just need to select the electrodes that we will keep (in elec_cell)
+    %%% and save the data into resultfile
+    
+    cfgpreproc.outputfile = resultfile;
+    cfgpreproc.channel = elec_cell;
+    
+    %%% Launch the preprocessing
+    
+    ft_preprocessing(cfgpreproc);
+    
+    
+end
