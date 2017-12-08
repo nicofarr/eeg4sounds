5442f515e0c385adf1076471d19724fd2158cedd
mmenoret <mathilde.menoret@telecom-bretagne.eu>
2017-12-08 17:22:11 +0100

    4b
    Il est possible d'inverser l'ordre (summary puis electrode ou l'inverse
    si tu préfères)

--- ecoute/script_4b_visu_data_segm.m
+++ ecoute/script_4b_visu_data_segm.m
@@ -8,54 +8,70 @@ check_set_resultdir;
 [filetoinspect,filepath] = uigetfile([resultdir '/ecoute/import/*.mat']);
 
 %%% Create result folder for storing final all artefacts
-if ~exist([resultdir '/ecoute/art_segm'],'dir')
+if ~exist([resultdir '/ecoute/art_elec'],'dir')
     
-    mkdir([resultdir '/ecoute/art_segm'])
+    mkdir([resultdir '/ecoute/art_elec'])
    
 end
 
-%%% Checking whether this file has been already inspected 
+%%% Checking whether this file has been already inspected and load 
+%%% bad & good electrodes list
 
-resultfile_art_segm = [resultdir, '/ecoute/art_segm/', filetoinspect];
+resultfile_elec = [resultdir, '/ecoute/art_elec/', filetoinspect];
 
-if exist(resultfile_art_segm,'file')
+visdata = load([filepath,filetoinspect],'data');
+list_elec=visdata.data.label;
+if exist(resultfile_elec,'file')
     disp('File has already been inspected : loading results.');
-    artfctdef_prev = load(resultfile_art_segm,'????');
+    artfctdef_prev = load(resultfile_elec,'elec');
+    list_elec=elec.good;
 end
 
-%%% Defining the cfg for inspection 
-cfg = [];
-visdata = load([filepath,filetoinspect],'data');
+
 % Load artifacts 
 resultfile_all_art = [resultdir '/ecoute/all_art/', filetoinspect];
 artfctdef_final = load(resultfile_all_art,'artfctdef');
 
+% Segment data
 cfg            = [];
-cfg.length    = 10; % SEGMENTS DE X SECONDES 
-data_cut=ft_redefinetrial(cfg,visdata.data); 
+cfg.length    = 5; % SEGMENTS DE X SECONDES 
+data_post_art=ft_redefinetrial(cfg,visdata.data); 
 
 cfg            = [];
 cfg.artfctdef=artfctdef_final.artfctdef;
-cfg.artfctdef.reject          = 'partial';% ou 'complete' si on supprime tout le segment
-cfg.artfctdef.minaccepttim    = 5;    % DUREE (en Secondes) MINIMALE TOLEREE                 
-data_post_art = ft_rejectartifact(cfg,data_cut);
+cfg.artfctdef.reject          = 'complete';% ou 'complete' si on supprime tout le segment
+data_post_art = ft_rejectartifact(cfg,data_post_art);
+for i=1:length(data_post_art.time)
+   data_post_art.time(i)=data_post_art.time(1);
+end
+% Il est possible d'inverser si on préfere electrode puis summary       
 
-cfg = [];
-cfg.method   = 'channel';
-%cfg.alim    = 100000;
-%cfg.eegscale    = 100;
-cfg.channel  = 'all';   
-data_clean   = ft_rejectvisual(cfg, data_post_art);  
-
-cfg = [];
-cfg.method   = 'trial';
-cfg.channel  = 'all';   
-data   = ft_rejectvisual(cfg, data_post_art);  
-
+% Affiche Summary
 cfg = [];
 cfg.method   = 'summary';% 'channel' 'trial'
-cfg.channel  = 'all';    % 
-data   = ft_rejectvisual(cfg, data_post_art);  
+data   = ft_rejectvisual(cfg, data_post_art); 
+
+% Affiche par electrodes 
+cfg = [];
+cfg.method   = 'channel';
+cfg.preproc.demean='yes';
+%cfg.alim    = 1000000000;
+%cfg.eegscale    = 100;
+cfg.channel  = list_elec;  
+%cfg.latency = [0 5];
+data   = ft_rejectvisual(cfg, data);
+
+
+% Enregistre dans "elec" le nom des electrodes "good" et "bad"
+chansel = false(1, length(data_post_art.label));
+chansel(match_str(data_post_art.label, data.label)) = true;
+removed = find(~chansel);
+elec.bad=data_post_art.label(removed);
+elec.good=data.label;
+
+save(resultfile_elec,'elec')
+
+
 
 
 
