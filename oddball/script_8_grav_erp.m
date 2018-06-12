42f3612a57a0f5a77ea06a96922a20ffdb9d81c7
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2018-06-12 17:24:16 +0200

    added save

--- oddball/script_8_grav_erp.m
+++ oddball/script_8_grav_erp.m
@@ -54,4 +54,22 @@ cfgplot.layout =  '../common/layout_E.mat';
 
 ft_multiplotER(cfgplot,grav_bin_std,grav_bin_dev,grav_ste_std,grav_ste_dev);
 
+%% Redo the grand averages just to save them, with the option that keeps individuals.
+
+cfg = [];
+cfg.keepindividual = 'yes';
+
+cfg.outputfile =  [resultdir, '/oddball/grav/grav_bin_std.mat'];
+
+grav_bin_std = ft_timelockgrandaverage(cfg,erp_bin_CP12.erp.std,erp_bin_CS07.erp.std,erp_bin_DC37.erp.std,erp_bin_EB39.erp.std,erp_bin_GN06.erp.std,erp_bin_HF38.erp.std,erp_bin_LA17.erp.std,erp_bin_LV18.erp.std,erp_bin_MJ05.erp.std,erp_bin_ML15.erp.std,erp_bin_MM10.erp.std,erp_bin_MM36.erp.std,erp_bin_NC28.erp.std,erp_bin_NH32.erp.std,erp_bin_PC27.erp.std,erp_bin_PL31.erp.std,erp_bin_RV26.erp.std,erp_bin_TA19.erp.std,erp_bin_VP03.erp.std);
+
+cfg.outputfile =  [resultdir, '/oddball/grav/grav_bin_dev.mat'];
+grav_bin_dev = ft_timelockgrandaverage(cfg,erp_bin_CP12.erp.dev,erp_bin_CS07.erp.dev,erp_bin_DC37.erp.dev,erp_bin_EB39.erp.dev,erp_bin_GN06.erp.dev,erp_bin_HF38.erp.dev,erp_bin_LA17.erp.dev,erp_bin_LV18.erp.dev,erp_bin_MJ05.erp.dev,erp_bin_ML15.erp.dev,erp_bin_MM10.erp.dev,erp_bin_MM36.erp.dev,erp_bin_NC28.erp.dev,erp_bin_NH32.erp.dev,erp_bin_PC27.erp.dev,erp_bin_PL31.erp.dev,erp_bin_RV26.erp.dev,erp_bin_TA19.erp.dev,erp_bin_VP03.erp.dev);
+
+cfg.outputfile =  [resultdir, '/oddball/grav/grav_ste_std.mat'];
+grav_ste_std = ft_timelockgrandaverage(cfg,erp_ste_CP12.erp.std,erp_ste_CS07.erp.std,erp_ste_DC37.erp.std,erp_ste_EB39.erp.std,erp_ste_GN06.erp.std,erp_ste_HF38.erp.std,erp_ste_LA17.erp.std,erp_ste_LV18.erp.std,erp_ste_MJ05.erp.std,erp_ste_ML15.erp.std,erp_ste_MM10.erp.std,erp_ste_MM36.erp.std,erp_ste_NC28.erp.std,erp_ste_NH32.erp.std,erp_ste_PC27.erp.std,erp_ste_PL31.erp.std,erp_ste_RV26.erp.std,erp_ste_TA19.erp.std,erp_ste_VP03.erp.std);
+
+
+cfg.outputfile =  [resultdir, '/oddball/grav/grav_ste_dev.mat'];
+grav_ste_dev = ft_timelockgrandaverage(cfg,erp_ste_CP12.erp.dev,erp_ste_CS07.erp.dev,erp_ste_DC37.erp.dev,erp_ste_EB39.erp.dev,erp_ste_GN06.erp.dev,erp_ste_HF38.erp.dev,erp_ste_LA17.erp.dev,erp_ste_LV18.erp.dev,erp_ste_MJ05.erp.dev,erp_ste_ML15.erp.dev,erp_ste_MM10.erp.dev,erp_ste_MM36.erp.dev,erp_ste_NC28.erp.dev,erp_ste_NH32.erp.dev,erp_ste_PC27.erp.dev,erp_ste_PL31.erp.dev,erp_ste_RV26.erp.dev,erp_ste_TA19.erp.dev,erp_ste_VP03.erp.dev);
 
